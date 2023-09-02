7a083e33539b6661361d0b55aa7f2eebf75424c8
Yoonyesol <maniac970430@naver.com>
2023-09-03 02:48:09 +0900

    항목 추가 버튼 이벤트 구현하기
    평소에는 입력form이 닫혀있다가 항목추가 버튼을 누르면 form이 나타나는 기능 구현

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   //enteredExpenseData: 하위 컴포넌트에서 받아온 값
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
@@ -11,11 +13,28 @@ const NewExpense = (props) => {
 
     //ExpenseForm 컴포넌트에서 이벤트 발생 시 실행되는 함수이므로 이 안에 App.js로 데이터를 보낼 함수를 실행시킨다.
     props.onAddExpense(expenseData);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}>Add New Expense</button>
+      )}
+      {isEditing && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={stopEditingHandler}
+        />
+      )}
     </div>
   );
 };
