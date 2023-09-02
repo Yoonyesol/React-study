1c6c09a4ade9c3f67cefff31d6ea4f817c1493fa
Yoonyesol <maniac970430@naver.com>
2023-09-03 01:50:33 +0900

    조건에 따라 콘텐츠 출력하기

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,11 +6,27 @@ import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
 
 const Expenses = ({ items }) => {
-  const [filterYear, setFilteredYear] = useState("2020");
+  const [filterYear, setFilteredYear] = useState("2023");
 
   const filterChangeHandler = (selecedYear) => {
     setFilteredYear(selecedYear);
   };
+
+  const filteredExpenses = items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filterYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div>
@@ -19,14 +35,7 @@ const Expenses = ({ items }) => {
           selected={filterYear}
           onChangeFilter={filterChangeHandler}
         />
-        {items.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
+        {expensesContent}
       </Card>
     </div>
   );
