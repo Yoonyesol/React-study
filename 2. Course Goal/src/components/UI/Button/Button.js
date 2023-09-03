bcf300c98b1eb58d75e17384657ac02ca483b7ef
Yoonyesol <maniac970430@naver.com>
2023-09-03 17:21:59 +0900

    CSS 모듈 사용하여 CSS 스타일링하기

--- 2. Course Goal/src/components/UI/Button/Button.js
+++ 2. Course Goal/src/components/UI/Button/Button.js
@@ -1,41 +1,41 @@
-import styled from "styled-components";
+// import styled from "styled-components";
 
-import "./Button.css";
+import styles from "./Button.module.css";
 
-const Button = styled.button`
-  width: 100%; //미디어쿼리 화면 사이즈에 속하지 않을 때 버튼 사이즈
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   width: 100%; //미디어쿼리 화면 사이즈에 속하지 않을 때 버튼 사이즈
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
 
-  //미디어쿼리(모바일 화면보다 큰 장치일 때)
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   //미디어쿼리(모바일 화면보다 큰 장치일 때)
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  // &:생성한 컴포넌트의 가상선택자(해당 요소가 존재한다면 실행)
-  &:focus {
-    outline: none;
-  }
+//   // &:생성한 컴포넌트의 가상선택자(해당 요소가 존재한다면 실행)
+//   &:focus {
+//     outline: none;
+//   }
 
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`;
+//   &:hover,
+//   &:active {
+//     background: #ac0e77;
+//     border-color: #ac0e77;
+//     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+//   }
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
