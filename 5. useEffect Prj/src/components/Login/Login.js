93d6694734a40354487b5c4fa2e33890c26b7d7d
Yoonyesol <maniac970430@naver.com>
2023-09-06 14:11:45 +0900

    useEffect에서 Cleanup 함수 사용하기

--- 5. useEffect Prj/src/components/Login/Login.js
+++ 5. useEffect Prj/src/components/Login/Login.js
@@ -12,9 +12,17 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
-    setFormIsValid(
-      enteredEmail.includes("@") && enteredPassword.trim().length > 6
-    );
+    const identifier = setTimeout(() => {
+      console.log("Checking form validity!");
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    return () => {
+      console.log("CLEANUP");
+      clearTimeout(identifier); //cleanup함수 실행 시마다 이전에 설정된 timer가 종료된다.
+    }; //Cleanup 함수, 컴포넌트가 재사용될 때마다 실행된다.(모든 새로운 사이드이펙트 함수가 실행되기 전에, 컴포넌트가 제거되기 전에 실행된다.)
   }, [enteredEmail, enteredPassword]); //사이드이펙트 함수에서 사용하는 것을 의존성으로 추가한다.(셋 중 하나가 변하면 함수 실행)
 
   const emailChangeHandler = (event) => {
