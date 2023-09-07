2d51098cf1ad55a0c7123264b791ee8803276fce
Yoonyesol <maniac970430@naver.com>
2023-09-07 16:08:11 +0900

    react partal을 통해 모달 추가하기

--- 6. Food Order App/src/components/UI/Modal.js
+++ 6. Food Order App/src/components/UI/Modal.js
@@ -1,6 +1,31 @@
 import classes from "./Modal.module.css";
+import { Fragment } from "react";
+import ReaceDOM from "react-dom";
+
+const Backdrop = (props) => {
+  return <div className={classes.backdrop} />;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
+const partalElement = document.getElementById("overlays"); //포털 위치(index.html)
 
 const Modal = (props) => {
-  return <div>{props.children}</div>;
+  return (
+    <Fragment>
+      {/* 포털대상, 어디로 이동시킬 것인지 위치 */}
+      {ReaceDOM.createPortal(<Backdrop />, partalElement)}
+      {ReaceDOM.createPortal(
+        <ModalOverlay>{props.children}</ModalOverlay>,
+        partalElement
+      )}
+    </Fragment>
+  );
 };
 export default Modal;
