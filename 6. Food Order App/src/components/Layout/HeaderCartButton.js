993b074d3a8025970366a82d5220b9a92ca17bb2
Yoonyesol <maniac970430@naver.com>
2023-09-08 13:47:15 +0900

    헤더 장바구니 - Context 사용

--- 6. Food Order App/src/components/Layout/HeaderCartButton.js
+++ 6. Food Order App/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,23 @@
+import { useContext } from "react";
 import CartIcon from "../Cart/CartIcon";
 import classes from "./HeaderCartButton.module.css";
+import CartContext from "../../store/cart-context";
 
 const HeaderCartButton = (props) => {
+  const carCtx = useContext(CartContext);
+
+  //reduce: 데이터 배열을 값 하나로 변환해 주는 메소드
+  const numberOfCarItems = carCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>Your Cart</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCarItems}</span>
     </button>
   );
 };
