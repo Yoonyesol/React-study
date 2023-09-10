21abbbb8dd6b863f4fcde933a0bcf3e1cc33e1ff
yesol <maniac970430@naver.com>
2023-09-11 00:58:19 +0900

    POST 요청 보내기(firebase 사용)

--- 8. HTTP Request Prj/src/App.js
+++ 8. HTTP Request Prj/src/App.js
@@ -2,6 +2,7 @@ import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,7 +14,9 @@ function App() {
     setIsLoading(true); //데이터 로딩중
     setError(null); //이전에 받았을지도 모르는 에러를 초기화
     try {
-      const response = await fetch("https://swapi.dev/api/films/");
+      const response = await fetch(
+        "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/movies.json"
+      );
       //정상적으로 데이터를 받아오지 못했을 때.
       //파싱 전에 오류 핸들러 처리해 주어야 한다.
       if (!response.ok) {
@@ -22,15 +25,19 @@ function App() {
 
       const data = await response.json();
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
+      //변환 코드
+      const loadedMovies = []; //내부에 객체가 있는 배열
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate,
+        });
+      }
+
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
@@ -41,6 +48,21 @@ function App() {
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler]); //함수가 변경되면 재실행
+
+  async function addMovieHandler(movie) {
+    const response = await fetch(
+      "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/movies.json",
+      {
+        method: "POST",
+        body: JSON.stringify(movie), //json형태로 만들어서 전송
+        headers: {
+          "Content-Type": "application/json", //헤더를 통해 어떤 컨텐츠가 전달되는지 알 수 있다
+        },
+      }
+    );
+    const data = await response.json();
+    console.log(data);
+  }
 
   let content = <p>Found no movies.</p>;
   if (movies.length > 0) {
@@ -57,6 +79,9 @@ function App() {
 
   return (
     <React.Fragment>
+      <section>
+        <AddMovie onAddMovie={addMovieHandler} />
+      </section>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
