1ce14fe24445a54a9c87f8302f84fe61f987a374
Yoonyesol <maniac970430@naver.com>
2023-09-10 18:22:30 +0900

    useCallback()으로 함수 재생성 방지하기, 의존성(클로저)

--- 7. React And Optimization/src/App.js
+++ 7. React And Optimization/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 
 import Button from "./components/UI/Button/Button";
 import "./App.css";
@@ -6,17 +6,30 @@ import DemoOutput from "./components/Demo/DemoOutput";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log("APP RUNNING"); //버튼 누를 때마다 출력됨 -> 리액트는 상태나 props, context 변경 시에만 함수를 재실행, 재평가한다.
 
-  const toggleParagraphHandler = () => {
-    setShowParagraph((prevshowParagraph) => !prevshowParagraph);
+  const toggleParagraphHandler = useCallback(() => {
+    //allowToggle: 자바스크립트 클로저, 함수 밖의 변수를 함수 안에서 사용 가능.
+    //변수의 값==변수가 저장된 시점의 값(함수 생성 시점의 값)
+    //useCallback때문에 외부 변수의 값이 변한다 해도(setState) 함수가 재생성되지는 않음.
+    //==allowToggle은 변경되기 이전의 값을 유지한다.
+    //따라서 항상 최신값을 유지하기 위해 의존성을 추가해 주어야 한다.
+    if (allowToggle) {
+      setShowParagraph((prevshowParagraph) => !prevshowParagraph);
+    }
+  }, [allowToggle]); //allowToggle이 변하지 않는 이상 이 함수도 실행이 안됨
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
   };
 
   return (
     <div className="app">
       <h1>Hi there!</h1>
-      <DemoOutput show={false} />
+      <DemoOutput show={showParagraph} />
+      <Button onClick={allowToggleHandler}>Allow Toggling</Button>
       <Button onClick={toggleParagraphHandler}>Show Paragraph!</Button>
     </div>
   );
