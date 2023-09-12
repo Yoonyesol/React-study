cc4d95bfd7cdc31a403348a52087c552e099c64b
yesol <maniac970430@naver.com>
2023-09-12 16:26:54 +0900

    페이로드 연결하기

--- 13. Redux-React/src/components/Counter.js
+++ 13. Redux-React/src/components/Counter.js
@@ -12,6 +12,10 @@ const Counter = () => {
     dispatch({ type: "increment" });
   };
 
+  const increseHandler = () => {
+    dispatch({ type: "increse", amount: 5 });
+  };
+
   const decrementHandler = () => {
     dispatch({ type: "decrement" });
   };
@@ -24,6 +28,7 @@ const Counter = () => {
       <div className={classes.value}>{counter}</div>
       <div>
         <button onClick={incrementHandler}>Increment</button>
+        <button onClick={increseHandler}>Increment by 5</button>
         <button onClick={decrementHandler}>Decrement</button>
       </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
