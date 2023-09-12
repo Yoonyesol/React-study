81ce2f0e3887740401ad26a7548d0c17b36ea128
yesol <maniac970430@naver.com>
2023-09-12 16:48:57 +0900

    여러 state 속성 작업하기

--- 13. Redux-React/src/components/Counter.js
+++ 13. Redux-React/src/components/Counter.js
@@ -1,15 +1,17 @@
 import { useDispatch, useSelector } from "react-redux";
 
 import classes from "./Counter.module.css";
+import { INCREMENT } from "../store/index";
 
 const Counter = () => {
   //useSelector: 저장소의 데이터에 접근하기(자동으로 구독 설정) -> 리덕스 저장소 내 데이터 변경 시 컴포넌트 함수 리렌더링
   const counter = useSelector((state) => state.counter);
   //redux store에 대한 action을 보내는 함수
   const dispatch = useDispatch();
+  const show = useSelector((state) => state.showCounter);
 
   const incrementHandler = () => {
-    dispatch({ type: "increment" });
+    dispatch({ type: INCREMENT });
   };
 
   const increseHandler = () => {
@@ -20,12 +22,14 @@ const Counter = () => {
     dispatch({ type: "decrement" });
   };
 
-  const toggleCounterHandler = () => {};
+  const toggleCounterHandler = () => {
+    dispatch({ type: "toggle" });
+  };
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>{counter}</div>
+      {show && <div className={classes.value}>{counter}</div>}
       <div>
         <button onClick={incrementHandler}>Increment</button>
         <button onClick={increseHandler}>Increment by 5</button>
--- 13. Redux-React/src/store/index.js
+++ 13. Redux-React/src/store/index.js
@@ -1,21 +1,39 @@
 import { createStore } from "redux";
 
+export const INCREMENT = "increment";
+
+const initialState = { counter: 0, showCounter: true };
+
 //리듀서 함수
-const counterReducer = (state = { counter: 0 }, action) => {
-  if (action.type === "increment") {
+const counterReducer = (state = initialState, action) => {
+  if (action.type === INCREMENT) {
     return {
+      //새로운 객체를 반환하는 것이므로 showCounter내용을 작성해 주지 않으면 새로운 객체가 기존 객체를 덮어써서 counter div가 아예 사라지게 된다.
+      //따라서 항상 새 값을 생성해 주어야 함.
       counter: state.counter + 1,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "increse") {
     return {
+      //반드시 새로운 객체를 생성해서 상태를 바꾸어야 함. 원본 state 상태를 직접 바꾸면 안된다.
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
+
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      counter: state.counter,
+      showCounter: !state.showCounter,
     };
   }
   return state;
