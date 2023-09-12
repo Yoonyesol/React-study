4c6a16d9f0c5b14fcc060520b6f928505155d94c
yesol <maniac970430@naver.com>
2023-09-12 11:04:54 +0900

    프로젝트6 음식주문 서비스에 HTTP fetch 기능 추가
    기존 더미데이터는 firebase로 이동시키고, 파이어베이스에서 데이터를 가져온다. 로딩과 오류 핸들링 처리도 함께 진행했다.

--- 6. Food Order App/src/components/Meals/AvailableMeals.js
+++ 6. Food Order App/src/components/Meals/AvailableMeals.js
@@ -1,36 +1,68 @@
+import { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import classes from "./AvailableMeals.module.css";
 import MealItem from "./MealItem/MealItem";
 
-const DUMMY_MEALS = [
-  {
-    id: "m1",
-    name: "Sushi",
-    description: "Finest fish and veggies",
-    price: 22.99,
-  },
-  {
-    id: "m2",
-    name: "Schnitzel",
-    description: "A german specialty!",
-    price: 16.5,
-  },
-  {
-    id: "m3",
-    name: "Barbecue Burger",
-    description: "American, raw, meaty",
-    price: 12.99,
-  },
-  {
-    id: "m4",
-    name: "Green Bowl",
-    description: "Healthy...and green...",
-    price: 18.99,
-  },
-];
-
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => (
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [httpError, setHttpError] = useState(null);
+
+  useEffect(() => {
+    //useEffect 내부에서는 프로미스 객체를 반환해서는 안 되므로, fetchMeals라는 새로운 함수를 내부에 만들어 실행한다.
+    const fetchMeals = async () => {
+      setIsLoading(true); //로딩중
+
+      const response = await fetch(
+        "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/meals.json"
+      );
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!"); //다음 줄이 실행되지 않음
+      }
+
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          name: responseData[key].name, //key아래 중첩객체에 접근
+          description: responseData[key].description,
+          price: responseData[key].price,
+        });
+      }
+
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    //호출 시도
+    fetchMeals().catch((error) => {
+      //에러발생 핸들러
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => (
     <MealItem
       id={meal.id}
       key={meal.id}
