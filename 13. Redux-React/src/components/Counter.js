926039949dea42cf87df0185df73dcd6b7fcec0f
yesol <maniac970430@naver.com>
2023-09-12 16:18:31 +0900

    리덕스 저장소 생성 및 저장소에 접근하여 데이터 가져오기

--- 13. Redux-React/src/components/Counter.js
+++ 13. Redux-React/src/components/Counter.js
@@ -1,12 +1,31 @@
-import classes from './Counter.module.css';
+import { useDispatch, useSelector } from "react-redux";
+
+import classes from "./Counter.module.css";
 
 const Counter = () => {
+  //useSelector: 저장소의 데이터에 접근하기(자동으로 구독 설정) -> 리덕스 저장소 내 데이터 변경 시 컴포넌트 함수 리렌더링
+  const counter = useSelector((state) => state.counter);
+  //redux store에 대한 action을 보내는 함수
+  const dispatch = useDispatch();
+
+  const incrementHandler = () => {
+    dispatch({ type: "increment" });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: "decrement" });
+  };
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
---
+++ 13. Redux-React/src/store/index.js
@@ -0,0 +1,21 @@
+import { createStore } from "redux";
+
+//리듀서 함수
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+  return state;
+};
+
+//저장소 생성
+const store = createStore(counterReducer);
+
+export default store;
