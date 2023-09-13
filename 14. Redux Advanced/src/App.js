22627debaa957e9483a53221f926745f3ae84599
yesol <maniac970430@naver.com>
2023-09-13 13:49:50 +0900

    리덕스와 useEffect를 함께 사용하기(firebase PUT)

--- 14. Redux Advanced/src/App.js
+++ 14. Redux Advanced/src/App.js
@@ -2,9 +2,21 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart); //cart 리덕스 스토어 구독
+
+  useEffect(() => {
+    fetch(
+      "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+      {
+        method: "PUT", //기존 데이터 오버라이드
+        body: JSON.stringify(cart),
+      }
+    );
+  }, [cart]); //구독한 cart의 상태가 바뀔 때마다 함수 실행
 
   return (
     <Layout>
