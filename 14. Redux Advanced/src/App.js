9bac599a8203fa79916314676c8697085bee494b
yesol <maniac970430@naver.com>
2023-09-13 14:47:58 +0900

    리덕스로 HTTP State  및 피드백 처리하기(Notification)

--- 14. Redux Advanced/src/App.js
+++ 14. Redux Advanced/src/App.js
@@ -1,28 +1,82 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
-import { useEffect } from "react";
+import { Fragment, useEffect } from "react";
+import { uiActions } from "./store/ui-slice";
+import Notification from "./components/UI/Notification";
+
+let isInitial = true;
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart); //cart 리덕스 스토어 구독
+  const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    fetch(
-      "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-      {
-        method: "PUT", //기존 데이터 오버라이드
-        body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      dispatch(
+        uiActions.showNotification({
+          status: "pending",
+          title: "Sending...",
+          message: "Sending cart data",
+        })
+      );
+
+      const response = await fetch(
+        "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT", //기존 데이터 오버라이드
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
       }
-    );
-  }, [cart]); //구독한 cart의 상태가 바뀔 때마다 함수 실행
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    };
+
+    //처음 로드할 때는 장바구니에 데이터 전송을 요청하지 않는다.
+    //처음 창을 로드할 때는 notification이 등장하지 않는다.
+    if (isInitial) {
+      isInitial = false;
+      return;
+    }
+
+    sendCartData().catch((error) => {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    });
+  }, [cart, dispatch]); //구독한 cart의 상태가 바뀔 때마다 함수 실행
 
   return (
-    <Layout>
-      {showCart && <Cart />}
-      <Products />
-    </Layout>
+    <Fragment>
+      {notification && (
+        <Notification
+          status={notification.status}
+          title={notification.title}
+          message={notification.message}
+        />
+      )}
+      <Layout>
+        {showCart && <Cart />}
+        <Products />
+      </Layout>
+    </Fragment>
   );
 }
 
--- 14. Redux Advanced/src/store/ui-slice.js
+++ 14. Redux Advanced/src/store/ui-slice.js
@@ -2,11 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const uiSlice = createSlice({
   name: "ui", //고유 이름
-  initialState: { cartIsVisible: false }, //초기 상태
+  initialState: { cartIsVisible: false, notification: null }, //초기 상태
   reducers: {
     //메소드 맵
     toggle(state) {
       state.cartIsVisible = !state.cartIsVisible;
+    },
+    showNotification(state, action) {
+      state.notification = {
+        status: action.payload.status,
+        title: action.payload.title,
+        message: action.payload.message,
+      };
     },
   },
 });
