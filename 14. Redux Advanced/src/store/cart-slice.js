8875ec57ec4ad47ff4ce29b03ca5895921d2ec8f
yesol <maniac970430@naver.com>
2023-09-13 11:45:11 +0900

    cart slice 적용

--- 14. Redux Advanced/src/store/cart-slice.js
+++ 14. Redux Advanced/src/store/cart-slice.js
@@ -5,18 +5,17 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0, //장바구니 항목의 총 수량
-    totalAmount: 0,
   },
   //함수 정의
   reducers: {
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
-
+      state.totalQuantity++;
       //이미 존재하는 품목
       if (!existingItem) {
         state.items.push({
-          itemId: newItem.id,
+          id: newItem.id,
           price: newItem.price,
           quantity: 1,
           totalPrice: newItem.price, //가격*수량
@@ -25,12 +24,13 @@ const cartSlice = createSlice({
       } else {
         //항목이 존재
         existingItem.quantity++;
-        existingItem.totalPrice = newItem.totalPrice + newItem.price;
+        existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
+      state.totalQuantity--;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
