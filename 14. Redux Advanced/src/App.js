915631fa50a6b613c477a4ba9f1ce2c61e6e7b8e
yesol <maniac970430@naver.com>
2023-09-13 15:08:39 +0900

    액션 생성자 Thunk 사용하기

--- 14. Redux Advanced/src/App.js
+++ 14. Redux Advanced/src/App.js
@@ -3,8 +3,8 @@ import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
 import { Fragment, useEffect } from "react";
-import { uiActions } from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import { sendCartData } from "./store/cart-slice";
 
 let isInitial = true;
 
@@ -15,36 +15,6 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: "pending",
-          title: "Sending...",
-          message: "Sending cart data",
-        })
-      );
-
-      const response = await fetch(
-        "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
-        {
-          method: "PUT", //기존 데이터 오버라이드
-          body: JSON.stringify(cart),
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Sending cart data failed.");
-      }
-
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success!",
-          message: "Sent cart data successfully!",
-        })
-      );
-    };
-
     //처음 로드할 때는 장바구니에 데이터 전송을 요청하지 않는다.
     //처음 창을 로드할 때는 notification이 등장하지 않는다.
     if (isInitial) {
@@ -52,15 +22,7 @@ function App() {
       return;
     }
 
-    sendCartData().catch((error) => {
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error!",
-          message: "Sending cart data failed!",
-        })
-      );
-    });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]); //구독한 cart의 상태가 바뀔 때마다 함수 실행
 
   return (
--- 14. Redux Advanced/src/store/cart-slice.js
+++ 14. Redux Advanced/src/store/cart-slice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { uiActions } from "./ui-slice";
 
 const cartSlice = createSlice({
   name: "cart",
@@ -40,6 +41,53 @@ const cartSlice = createSlice({
     },
   },
 });
+
+//다른 함수를 반환하는 작업 가능
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "pending",
+        title: "Sending...",
+        message: "Sending cart data",
+      })
+    );
+
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://react-http-f5d7d-default-rtdb.europe-west1.firebasedatabase.app/cart.json",
+        {
+          method: "PUT", //기존 데이터 오버라이드
+          body: JSON.stringify(cart),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+
+    try {
+      await sendRequest();
+
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success!",
+          message: "Sent cart data successfully!",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error!",
+          message: "Sending cart data failed!",
+        })
+      );
+    }
+  };
+};
 
 export const cartActions = cartSlice.actions;
 
