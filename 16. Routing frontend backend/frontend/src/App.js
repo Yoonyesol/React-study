783940a8e7a26bf996ce1c6bd07472c9bcc11605
yesol <maniac970430@naver.com>
2023-09-14 15:54:20 +0900

    action() 함수 사용하여 게시물 등록하기

--- 16. Routing frontend backend/frontend/src/App.js
+++ 16. Routing frontend backend/frontend/src/App.js
@@ -1,7 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Event";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import EventDetailPage, {
   loader as eventDetailLoader,
 } from "./pages/EventDetail";
@@ -42,7 +42,7 @@ const router = createBrowserRouter([
             ],
           },
 
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
--- 16. Routing frontend backend/frontend/src/pages/NewEvent.js
+++ 16. Routing frontend backend/frontend/src/pages/NewEvent.js
@@ -1,4 +1,33 @@
+import { json, redirect } from "react-router-dom";
+import EventForm from "../components/EventForm";
+
 function NewEventPage() {
-  return <h1>NewEventPage</h1>;
+  return <EventForm />;
 }
 export default NewEventPage;
+
+//브라우저에서 실행되는 코드이므로 어떤 브라우저 api에도 접근할 수 있다.
+export async function action({ request, params }) {
+  const data = await request.formData(); //form의 데이터에 접근
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    description: data.get("description"),
+    date: data.get("date"),
+  };
+
+  const response = await fetch("http://localhost:8080/events", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+
+  return redirect("/events"); //다른 페이지로 사용자를 리디렉션
+}
